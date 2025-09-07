2cc751932204e9977d6311aecb1980c3bc836f83
Adet <>
2025-09-07 17:13:22 +0700

    test: migrate AuthControllerTest to @SpringBootTest with @AutoConfigureMockMvc
    - replaced @WebMvcTest with @SpringBootTest to avoid JPA metamodel error
    - enabled @AutoConfigureMockMvc for controller endpoint testing
    - allows full Spring context loading (JPA, Flyway, etc.) during tests

--- src/test/java/com/github/adetiamarhadi/xyz_auth_service/controller/AuthControllerTest.java
+++ src/test/java/com/github/adetiamarhadi/xyz_auth_service/controller/AuthControllerTest.java
@@ -10,16 +10,18 @@ import com.github.adetiamarhadi.xyz_auth_service.dto.SignupRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(AuthController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class AuthControllerTest {
 
     @Autowired
